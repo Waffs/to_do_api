8999d2ef0cda3601af1d04b0a47b86ace27dd222
Ahmad Wafiq Sa'ad <150338625+Waffs@users.noreply.github.com>
2025-01-09 12:02:32 +0100

    Add files via upload

---
+++ models.py
@@ -0,0 +1,21 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(255), nullable=True)
+    due_date = db.Column(db.DateTime, nullable=True)
+    priority = db.Column(db.String(10), nullable=False, default="Medium")  # Low, Medium, High
+    completed = db.Column(db.Boolean, default=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "due_date": self.due_date,
+            "priority": self.priority,
+            "completed": self.completed
+        }
